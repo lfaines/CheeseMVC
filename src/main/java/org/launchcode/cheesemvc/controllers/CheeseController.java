7384574aea53dec49b452614814979a66dfed81d
Blessed <codingdreams4ever@gmail.com>
2019-04-02 13:01:01 -0500

    added validation

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,70 +1,65 @@
 package org.launchcode.cheesemvc.controllers;
 
 import org.launchcode.cheesemvc.models.Cheese;
+import org.launchcode.cheesemvc.models.CheeseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+import javax.validation.Valid;
 
+/**
+ * Created by LaunchCode
+ */
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<Cheese> cheeses = new ArrayList<>();
-    //static HashMap<String, String> cheeses = new HashMap<>();
-
-    //Request path: /cheese
+    // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
-        model.addAttribute("cheeses", cheeses);
+
+        model.addAttribute("cheeses", CheeseData.getAll());
         model.addAttribute("title", "My Cheeses");
+
         return "cheese/index";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddCheeseForm(Model model) {
         model.addAttribute("title", "Add Cheese");
-       // model.addAttribute("title", "Add Description");
+        model.addAttribute(new Cheese()); //use properties of object to render form properly
         return "cheese/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
-        Cheese cheese = new Cheese(cheeseName, cheeseDescription);
-        cheeses.add(cheese);
-
-        //cheeses.put(cheeseName, cheeseDescription);
-
-        //cheeses.put(cheeseDescription);
-        //Redirect to /cheese
+    public String processAddCheeseForm(Model model, @ModelAttribute  @Valid Cheese newCheese, Errors errors) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Cheese");
+            return "cheese/add";
+        }
+        CheeseData.add(newCheese);
         return "redirect:";
-
     }
 
     @RequestMapping(value = "delete", method = RequestMethod.GET)
-    public String displayDeleteCheeseForm(Model model) {
-        model.addAttribute("cheeses", cheeses);
-        model.addAttribute("title", "Delete Cheese");
+    public String displayRemoveCheeseForm(Model model) {
+        model.addAttribute("cheeses", CheeseData.getAll());
+        model.addAttribute("title", "Remove Cheese");
         return "cheese/delete";
     }
 
     @RequestMapping(value = "delete", method = RequestMethod.POST)
-    public String processDeleteCheeseForm(@RequestParam ArrayList<Integer> cheeseIds) {
-        for (Integer cheeseId : cheeseIds) {
-            for (Cheese cheese: cheeses) {
-                if (cheese.getId() == cheeseId) {
-                    cheeses.remove(cheese);
-                    break;
-                }
-            }
+    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds) {
+
+        for (int cheeseId : cheeseIds) {
+            CheeseData.remove(cheeseId);
         }
         return "redirect:";
     }
-
-
-
 
 }
--- src/main/java/org/launchcode/cheesemvc/models/Cheese.java
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -1,21 +1,46 @@
 package org.launchcode.cheesemvc.models;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Cheese {
-    private int id;
+    //private int id;
+    @NotNull
+    @Size(min=3, max=15, message = "Name field must be between 3 and 15 characters.") //length of cheese name specified for validation
     private String name;
+
+    @NotNull
+    @Size(min=3, max=25, message = "Description field must be between 3 and 25 characters.")
     private String description;
-    private static int nextId = 0;
+
+    private int cheeseId;
+    //private static int nextId = 0;
+    private static int nextId = 1;
 
     public Cheese(String name, String description) {
-        this.id = nextId;
+        //this.id = nextId;
+        this(); //call the default constructor for the given class
         this.name = name;
         this.description = description;
+        //nextId++;
+    }
+
+    public Cheese() {
+        cheeseId = nextId;
         nextId++;
     }
 
-    public int getId() {
-        return id;
+    public int getCheeseId() {
+        return cheeseId;
     }
+
+    public void setCheeseId(int cheeseId) {
+        this.cheeseId = cheeseId;
+    }
+
+    //public int getId() {
+      //  return id;
+    //}
 
     public String getName() {
         return name;
